341e5f25a12585f8ac26613471c635847ea79f78
JanisKlevs <janis.klevs@gmail.com>
2018-11-05 16:36:20 +0200

    my first commit

---
+++ src/test/java/gaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package gaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAndroid ()
+	
+	{
+		System.out.println("NativeAndroid");
+	}
+
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/gaclickacademy/RESTAPITest.java
@@ -0,0 +1,20 @@
+package gaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void REST1 ()
+	
+	{
+		System.out.println("REST1");
+	}
+
+	@Test
+	public void REST2 ()
+	{
+		System.out.println("REST2");
+	}
+	
+}
